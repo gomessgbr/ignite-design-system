4742ef0daa63763d3c5212f1af2eb1e071212728
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-06-28 17:32:09 -0300

    feat: first component button

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,19 +1,19 @@
-import {  Button,  ButtonProps } from "@ignite-ui/react";
-import type {  Meta,  StoryObj } from "@storybook/react";
+import { Button, ButtonProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
 export default {
-  title: "Button",
+  title: 'Button',
   component: Button,
 
   args: {
-    children: "Enviar",
+    children: 'Enviar',
   },
-} as Meta<ButtonProps>;
+} as Meta<ButtonProps>
 
-export const Primary: StoryObj<ButtonProps> = {};
+export const Primary: StoryObj<ButtonProps> = {}
 
 export const Big: StoryObj<ButtonProps> = {
   args: {
-    size: "small",
+    size: 'big',
   },
-};
+}
