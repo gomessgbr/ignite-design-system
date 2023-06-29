7dda0aff036804e6d3a35edd99f22172fc6e353a
Gabriel Gomes <gabrielgomes98@gmail.com>
2023-06-28 21:14:13 -0300

    feat: make a avatar component

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -1,25 +1,27 @@
 "use strict";
+var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __defProps = Object.defineProperties;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __propIsEnum = Object.prototype.propertyIsEnumerable;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
-var __spreadValues = (a, b) => {
+var __spreadValues = (a2, b) => {
   for (var prop in b || (b = {}))
     if (__hasOwnProp.call(b, prop))
-      __defNormalProp(a, prop, b[prop]);
+      __defNormalProp(a2, prop, b[prop]);
   if (__getOwnPropSymbols)
     for (var prop of __getOwnPropSymbols(b)) {
       if (__propIsEnum.call(b, prop))
-        __defNormalProp(a, prop, b[prop]);
+        __defNormalProp(a2, prop, b[prop]);
     }
-  return a;
+  return a2;
 };
-var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
+var __spreadProps = (a2, b) => __defProps(a2, __getOwnPropDescs(b));
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, { get: all[name], enumerable: true });
@@ -32,11 +34,20 @@ var __copyProps = (to, from, except, desc) => {
   }
   return to;
 };
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
 // src/index.tsx
 var src_exports = {};
 __export(src_exports, {
+  Avatar: () => Avatar2,
   Box: () => Box,
   Heading: () => Heading,
   Text: () => Text
@@ -201,8 +212,172 @@ var Heading = styled("h2", {
     size: "md"
   }
 });
+
+// src/components/Avatar/index.tsx
+var import_react5 = __toESM(require("react"));
+
+// src/components/Avatar/styles.ts
+var Avatar = __toESM(require("@radix-ui/react-avatar"));
+var AvatarContainer = styled(Avatar.Root, {
+  borderRadius: "$full",
+  display: "inline-block",
+  width: "$12",
+  height: "$12",
+  overflow: "hidden"
+});
+var AvatarImage = styled(Avatar.Image, {
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+  borderRadius: "inherit"
+});
+var AvatarFallback = styled(Avatar.Fallback, {
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "$gray600",
+  color: "$gray800",
+  svg: {
+    width: "$6",
+    height: "$6"
+  }
+});
+
+// ../../node_modules/@phosphor-icons/react/dist/lib/context.es.js
+var import_react2 = require("react");
+var o = (0, import_react2.createContext)({
+  color: "currentColor",
+  size: "1em",
+  weight: "regular",
+  mirrored: false
+});
+
+// ../../node_modules/@phosphor-icons/react/dist/lib/IconBase.es.js
+var import_react3 = __toESM(require("react"), 1);
+var R = Object.defineProperty;
+var l = Object.getOwnPropertySymbols;
+var f = Object.prototype.hasOwnProperty;
+var g = Object.prototype.propertyIsEnumerable;
+var d = (t, r3, e2) => r3 in t ? R(t, r3, { enumerable: true, configurable: true, writable: true, value: e2 }) : t[r3] = e2;
+var s = (t, r3) => {
+  for (var e2 in r3 || (r3 = {}))
+    f.call(r3, e2) && d(t, e2, r3[e2]);
+  if (l)
+    for (var e2 of l(r3))
+      g.call(r3, e2) && d(t, e2, r3[e2]);
+  return t;
+};
+var a = (t, r3) => {
+  var e2 = {};
+  for (var o3 in t)
+    f.call(t, o3) && r3.indexOf(o3) < 0 && (e2[o3] = t[o3]);
+  if (t != null && l)
+    for (var o3 of l(t))
+      r3.indexOf(o3) < 0 && g.call(t, o3) && (e2[o3] = t[o3]);
+  return e2;
+};
+var P = (0, import_react3.forwardRef)((t, r3) => {
+  const m2 = t, {
+    alt: e2,
+    color: o3,
+    size: n2,
+    weight: c2,
+    mirrored: h2,
+    children: p2,
+    weights: u
+  } = m2, C = a(m2, [
+    "alt",
+    "color",
+    "size",
+    "weight",
+    "mirrored",
+    "children",
+    "weights"
+  ]), x = (0, import_react3.useContext)(o), {
+    color: v = "currentColor",
+    size: i,
+    weight: B = "regular",
+    mirrored: I = false
+  } = x, E2 = a(x, [
+    "color",
+    "size",
+    "weight",
+    "mirrored"
+  ]);
+  return /* @__PURE__ */ import_react3.default.createElement(
+    "svg",
+    s(s({
+      ref: r3,
+      xmlns: "http://www.w3.org/2000/svg",
+      width: n2 != null ? n2 : i,
+      height: n2 != null ? n2 : i,
+      fill: o3 != null ? o3 : v,
+      viewBox: "0 0 256 256",
+      transform: h2 || I ? "scale(-1, 1)" : void 0
+    }, E2), C),
+    !!e2 && /* @__PURE__ */ import_react3.default.createElement("title", null, e2),
+    p2,
+    u.get(c2 != null ? c2 : B)
+  );
+});
+P.displayName = "IconBase";
+
+// ../../node_modules/@phosphor-icons/react/dist/icons/User.es.js
+var import_react4 = __toESM(require("react"), 1);
+var o2 = Object.defineProperty;
+var p = Object.defineProperties;
+var E = Object.getOwnPropertyDescriptors;
+var n = Object.getOwnPropertySymbols;
+var s2 = Object.prototype.hasOwnProperty;
+var M2 = Object.prototype.propertyIsEnumerable;
+var r2 = (t, a2, l2) => a2 in t ? o2(t, a2, { enumerable: true, configurable: true, writable: true, value: l2 }) : t[a2] = l2;
+var c = (t, a2) => {
+  for (var l2 in a2 || (a2 = {}))
+    s2.call(a2, l2) && r2(t, l2, a2[l2]);
+  if (n)
+    for (var l2 of n(a2))
+      M2.call(a2, l2) && r2(t, l2, a2[l2]);
+  return t;
+};
+var m = (t, a2) => p(t, E(a2));
+var h = /* @__PURE__ */ new Map([
+  [
+    "bold",
+    /* @__PURE__ */ import_react4.default.createElement(import_react4.default.Fragment, null, /* @__PURE__ */ import_react4.default.createElement("path", { d: "M234.38,210a123.36,123.36,0,0,0-60.78-53.23,76,76,0,1,0-91.2,0A123.36,123.36,0,0,0,21.62,210a12,12,0,1,0,20.77,12c18.12-31.32,50.12-50,85.61-50s67.49,18.69,85.61,50a12,12,0,0,0,20.77-12ZM76,96a52,52,0,1,1,52,52A52.06,52.06,0,0,1,76,96Z" }))
+  ],
+  [
+    "duotone",
+    /* @__PURE__ */ import_react4.default.createElement(import_react4.default.Fragment, null, /* @__PURE__ */ import_react4.default.createElement("path", { d: "M192,96a64,64,0,1,1-64-64A64,64,0,0,1,192,96Z", opacity: "0.2" }), /* @__PURE__ */ import_react4.default.createElement("path", { d: "M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z" }))
+  ],
+  [
+    "fill",
+    /* @__PURE__ */ import_react4.default.createElement(import_react4.default.Fragment, null, /* @__PURE__ */ import_react4.default.createElement("path", { d: "M230.93,220a8,8,0,0,1-6.93,4H32a8,8,0,0,1-6.92-12c15.23-26.33,38.7-45.21,66.09-54.16a72,72,0,1,1,73.66,0c27.39,8.95,50.86,27.83,66.09,54.16A8,8,0,0,1,230.93,220Z" }))
+  ],
+  [
+    "light",
+    /* @__PURE__ */ import_react4.default.createElement(import_react4.default.Fragment, null, /* @__PURE__ */ import_react4.default.createElement("path", { d: "M229.19,213c-15.81-27.32-40.63-46.49-69.47-54.62a70,70,0,1,0-63.44,0C67.44,166.5,42.62,185.67,26.81,213a6,6,0,1,0,10.38,6C56.4,185.81,90.34,166,128,166s71.6,19.81,90.81,53a6,6,0,1,0,10.38-6ZM70,96a58,58,0,1,1,58,58A58.07,58.07,0,0,1,70,96Z" }))
+  ],
+  [
+    "regular",
+    /* @__PURE__ */ import_react4.default.createElement(import_react4.default.Fragment, null, /* @__PURE__ */ import_react4.default.createElement("path", { d: "M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z" }))
+  ],
+  [
+    "thin",
+    /* @__PURE__ */ import_react4.default.createElement(import_react4.default.Fragment, null, /* @__PURE__ */ import_react4.default.createElement("path", { d: "M227.46,214c-16.52-28.56-43-48.06-73.68-55.09a68,68,0,1,0-51.56,0c-30.64,7-57.16,26.53-73.68,55.09a4,4,0,0,0,6.92,4C55,184.19,89.62,164,128,164s73,20.19,92.54,54a4,4,0,0,0,3.46,2,3.93,3.93,0,0,0,2-.54A4,4,0,0,0,227.46,214ZM68,96a60,60,0,1,1,60,60A60.07,60.07,0,0,1,68,96Z" }))
+  ]
+]);
+var g2 = (0, import_react4.forwardRef)((t, a2) => /* @__PURE__ */ import_react4.default.createElement(P, m(c({ ref: a2 }, t), { weights: h })));
+g2.displayName = "User";
+
+// src/components/Avatar/index.tsx
+function Avatar2(props) {
+  return /* @__PURE__ */ import_react5.default.createElement(AvatarContainer, null, /* @__PURE__ */ import_react5.default.createElement(AvatarImage, __spreadValues({}, props)), /* @__PURE__ */ import_react5.default.createElement(AvatarFallback, { delayMs: 600 }, /* @__PURE__ */ import_react5.default.createElement(g2, null)));
+}
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
+  Avatar,
   Box,
   Heading,
   Text
--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -5,18 +5,18 @@ var __getOwnPropSymbols = Object.getOwnPropertySymbols;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __propIsEnum = Object.prototype.propertyIsEnumerable;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
-var __spreadValues = (a, b) => {
+var __spreadValues = (a2, b) => {
   for (var prop in b || (b = {}))
     if (__hasOwnProp.call(b, prop))
-      __defNormalProp(a, prop, b[prop]);
+      __defNormalProp(a2, prop, b[prop]);
   if (__getOwnPropSymbols)
     for (var prop of __getOwnPropSymbols(b)) {
       if (__propIsEnum.call(b, prop))
-        __defNormalProp(a, prop, b[prop]);
+        __defNormalProp(a2, prop, b[prop]);
     }
-  return a;
+  return a2;
 };
-var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
+var __spreadProps = (a2, b) => __defProps(a2, __getOwnPropDescs(b));
 
 // ../tokens/dist/index.mjs
 var colors = {
@@ -176,7 +176,171 @@ var Heading = styled("h2", {
     size: "md"
   }
 });
+
+// src/components/Avatar/index.tsx
+import React from "react";
+
+// src/components/Avatar/styles.ts
+import * as Avatar from "@radix-ui/react-avatar";
+var AvatarContainer = styled(Avatar.Root, {
+  borderRadius: "$full",
+  display: "inline-block",
+  width: "$12",
+  height: "$12",
+  overflow: "hidden"
+});
+var AvatarImage = styled(Avatar.Image, {
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+  borderRadius: "inherit"
+});
+var AvatarFallback = styled(Avatar.Fallback, {
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "$gray600",
+  color: "$gray800",
+  svg: {
+    width: "$6",
+    height: "$6"
+  }
+});
+
+// ../../node_modules/@phosphor-icons/react/dist/lib/context.es.js
+import { createContext as r } from "react";
+var o = r({
+  color: "currentColor",
+  size: "1em",
+  weight: "regular",
+  mirrored: false
+});
+
+// ../../node_modules/@phosphor-icons/react/dist/lib/IconBase.es.js
+import w, { forwardRef as y, useContext as M } from "react";
+var R = Object.defineProperty;
+var l = Object.getOwnPropertySymbols;
+var f = Object.prototype.hasOwnProperty;
+var g = Object.prototype.propertyIsEnumerable;
+var d = (t, r3, e2) => r3 in t ? R(t, r3, { enumerable: true, configurable: true, writable: true, value: e2 }) : t[r3] = e2;
+var s = (t, r3) => {
+  for (var e2 in r3 || (r3 = {}))
+    f.call(r3, e2) && d(t, e2, r3[e2]);
+  if (l)
+    for (var e2 of l(r3))
+      g.call(r3, e2) && d(t, e2, r3[e2]);
+  return t;
+};
+var a = (t, r3) => {
+  var e2 = {};
+  for (var o3 in t)
+    f.call(t, o3) && r3.indexOf(o3) < 0 && (e2[o3] = t[o3]);
+  if (t != null && l)
+    for (var o3 of l(t))
+      r3.indexOf(o3) < 0 && g.call(t, o3) && (e2[o3] = t[o3]);
+  return e2;
+};
+var P = y((t, r3) => {
+  const m2 = t, {
+    alt: e2,
+    color: o3,
+    size: n2,
+    weight: c2,
+    mirrored: h2,
+    children: p2,
+    weights: u
+  } = m2, C = a(m2, [
+    "alt",
+    "color",
+    "size",
+    "weight",
+    "mirrored",
+    "children",
+    "weights"
+  ]), x = M(o), {
+    color: v = "currentColor",
+    size: i,
+    weight: B = "regular",
+    mirrored: I = false
+  } = x, E2 = a(x, [
+    "color",
+    "size",
+    "weight",
+    "mirrored"
+  ]);
+  return /* @__PURE__ */ w.createElement(
+    "svg",
+    s(s({
+      ref: r3,
+      xmlns: "http://www.w3.org/2000/svg",
+      width: n2 != null ? n2 : i,
+      height: n2 != null ? n2 : i,
+      fill: o3 != null ? o3 : v,
+      viewBox: "0 0 256 256",
+      transform: h2 || I ? "scale(-1, 1)" : void 0
+    }, E2), C),
+    !!e2 && /* @__PURE__ */ w.createElement("title", null, e2),
+    p2,
+    u.get(c2 != null ? c2 : B)
+  );
+});
+P.displayName = "IconBase";
+
+// ../../node_modules/@phosphor-icons/react/dist/icons/User.es.js
+import e, { forwardRef as d2 } from "react";
+var o2 = Object.defineProperty;
+var p = Object.defineProperties;
+var E = Object.getOwnPropertyDescriptors;
+var n = Object.getOwnPropertySymbols;
+var s2 = Object.prototype.hasOwnProperty;
+var M2 = Object.prototype.propertyIsEnumerable;
+var r2 = (t, a2, l2) => a2 in t ? o2(t, a2, { enumerable: true, configurable: true, writable: true, value: l2 }) : t[a2] = l2;
+var c = (t, a2) => {
+  for (var l2 in a2 || (a2 = {}))
+    s2.call(a2, l2) && r2(t, l2, a2[l2]);
+  if (n)
+    for (var l2 of n(a2))
+      M2.call(a2, l2) && r2(t, l2, a2[l2]);
+  return t;
+};
+var m = (t, a2) => p(t, E(a2));
+var h = /* @__PURE__ */ new Map([
+  [
+    "bold",
+    /* @__PURE__ */ e.createElement(e.Fragment, null, /* @__PURE__ */ e.createElement("path", { d: "M234.38,210a123.36,123.36,0,0,0-60.78-53.23,76,76,0,1,0-91.2,0A123.36,123.36,0,0,0,21.62,210a12,12,0,1,0,20.77,12c18.12-31.32,50.12-50,85.61-50s67.49,18.69,85.61,50a12,12,0,0,0,20.77-12ZM76,96a52,52,0,1,1,52,52A52.06,52.06,0,0,1,76,96Z" }))
+  ],
+  [
+    "duotone",
+    /* @__PURE__ */ e.createElement(e.Fragment, null, /* @__PURE__ */ e.createElement("path", { d: "M192,96a64,64,0,1,1-64-64A64,64,0,0,1,192,96Z", opacity: "0.2" }), /* @__PURE__ */ e.createElement("path", { d: "M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z" }))
+  ],
+  [
+    "fill",
+    /* @__PURE__ */ e.createElement(e.Fragment, null, /* @__PURE__ */ e.createElement("path", { d: "M230.93,220a8,8,0,0,1-6.93,4H32a8,8,0,0,1-6.92-12c15.23-26.33,38.7-45.21,66.09-54.16a72,72,0,1,1,73.66,0c27.39,8.95,50.86,27.83,66.09,54.16A8,8,0,0,1,230.93,220Z" }))
+  ],
+  [
+    "light",
+    /* @__PURE__ */ e.createElement(e.Fragment, null, /* @__PURE__ */ e.createElement("path", { d: "M229.19,213c-15.81-27.32-40.63-46.49-69.47-54.62a70,70,0,1,0-63.44,0C67.44,166.5,42.62,185.67,26.81,213a6,6,0,1,0,10.38,6C56.4,185.81,90.34,166,128,166s71.6,19.81,90.81,53a6,6,0,1,0,10.38-6ZM70,96a58,58,0,1,1,58,58A58.07,58.07,0,0,1,70,96Z" }))
+  ],
+  [
+    "regular",
+    /* @__PURE__ */ e.createElement(e.Fragment, null, /* @__PURE__ */ e.createElement("path", { d: "M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z" }))
+  ],
+  [
+    "thin",
+    /* @__PURE__ */ e.createElement(e.Fragment, null, /* @__PURE__ */ e.createElement("path", { d: "M227.46,214c-16.52-28.56-43-48.06-73.68-55.09a68,68,0,1,0-51.56,0c-30.64,7-57.16,26.53-73.68,55.09a4,4,0,0,0,6.92,4C55,184.19,89.62,164,128,164s73,20.19,92.54,54a4,4,0,0,0,3.46,2,3.93,3.93,0,0,0,2-.54A4,4,0,0,0,227.46,214ZM68,96a60,60,0,1,1,60,60A60.07,60.07,0,0,1,68,96Z" }))
+  ]
+]);
+var g2 = d2((t, a2) => /* @__PURE__ */ e.createElement(P, m(c({ ref: a2 }, t), { weights: h })));
+g2.displayName = "User";
+
+// src/components/Avatar/index.tsx
+function Avatar2(props) {
+  return /* @__PURE__ */ React.createElement(AvatarContainer, null, /* @__PURE__ */ React.createElement(AvatarImage, __spreadValues({}, props)), /* @__PURE__ */ React.createElement(AvatarFallback, { delayMs: 600 }, /* @__PURE__ */ React.createElement(g2, null)));
+}
 export {
+  Avatar2 as Avatar,
   Box,
   Heading,
   Text
